db14ae8884f25bf65d14a893d6f4023b6317fa0f
emilly <alexiabriggs231@gmail.com>
2022-02-21 00:44:36 +0100

    b

---
+++ script.js
@@ -0,0 +1,18 @@
+const icon = document.querySelector(".fa-circle-exclamation");
+const post = document.querySelector(".op");
+const times = document.querySelector(".fa-circle-xmark");
+
+icon.addEventListener("click", function () {
+  post.classList.remove("hidden");
+});
+
+times.addEventListener("click", function () {
+  post.classList.add("hidden");
+  backgroundOverlay.classList.add("hidden");
+});
+const backgroundOverlay = document.querySelector(".background-overlay");
+
+icon.addEventListener("click", function () {
+  post.classList.remove("hidden");
+  backgroundOverlay.classList.remove("hidden");
+});
